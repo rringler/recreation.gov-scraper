76a761fbcdea98bf76a580754d7821f9d6674eab
Ryan Ringler <rringler@gmail.com>
2014-08-11 21:05:05 -0700

    Changes default purge threshold from 10 days to 3 days

--- models/query.rb
+++ models/query.rb
@@ -2,5 +2,5 @@ class Query < ActiveRecord::Base
   has_many :availabilities
 
   scope :latest, -> { order(created_at: :desc).limit(1).first }
-  scope :older,  -> { where(["created_at < ?", 10.days.ago]) }
+  scope :older,  ->(n = 3) { where(["created_at < ?", n.days.ago]) }
 end
